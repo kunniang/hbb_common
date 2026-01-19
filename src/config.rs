16254f0d2d51406557d946e2480640c9258c6785
zfen2016-dev <zfen2016@gmail.com>
2026-01-19 09:41:05 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["172.22.1.177"];
+pub const RS_PUB_KEY: &str = "ocK1B9LN1WXOquyDEwJvigpBczvyWX9sfjU+XcKLyos=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
